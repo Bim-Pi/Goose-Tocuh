86df78f52dd0000b62a8a2b59ee08894fad1674b
jason-cai538 <caij95@mcmaster.ca>
2026-02-13 15:24:48 -0500

    changing

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
     const image = document.getElementById("goose-image");
     let click = 0;
     
     image.addEventListener('click', function(){
         click += 1;
         console.log('Image was clicked ' + click + ' times');
+    });
+
+    let helpButton = document.getElementById("help-button");
+    let helpModal = document.getElementById("help-modal");
+    let closeHelpButton = document.getElementById("close-help");
+
+    helpButton.addEventListener('click', function() {
+        helpModal.style.display = "block";
+    });
+    
+    closeHelpButton.addEventListener('click', function() {
+        helpModal.style.display = "none";
     });
 });
