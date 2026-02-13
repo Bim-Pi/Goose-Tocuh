14c9deb93f1a2064a5e87b09f4d1faea3b65ea7f
Pimentel Brian Miguel <pimenteb@mcamster.ca>
2026-02-13 15:56:39 -0500

    added the script

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     const image = document.getElementById("goose-image");
     let click = 0;
-    
-    image.addEventListener('click', function(){
+    const honks = this.document.getElementById("resource-count")
+
+    image.addEventListener('click', function () {
         click += 1;
-        console.log('Image was clicked ' + click + ' times');
+
     });
 
     let helpButton = document.getElementById("help-button");
     let helpModal = document.getElementById("help-modal");
     let closeHelpButton = document.getElementById("close-help");
 
-    helpButton.addEventListener('click', function() {
+    helpButton.addEventListener('click', function () {
         helpModal.style.display = "block";
     });
-    
-    closeHelpButton.addEventListener('click', function() {
+
+    closeHelpButton.addEventListener('click', function () {
         helpModal.style.display = "none";
     });
 });
